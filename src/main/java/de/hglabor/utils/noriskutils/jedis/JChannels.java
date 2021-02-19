782a92e6066bbdf2ce472d1dfcd1e28e3193a4a5
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-02-19 22:47:39 +0100

    Added new JChannel HGQUEUE-REMOVE...

--- src/main/java/de/hglabor/utils/noriskutils/jedis/JChannels.java
+++ src/main/java/de/hglabor/utils/noriskutils/jedis/JChannels.java
@@ -4,4 +4,5 @@ public interface JChannels {
     String HGQUEUE_LEAVE = "hgqueue-leave";
     String HGQUEUE_JOIN = "hgqueue-join";
     String HGQUEUE_MOVE = "hgqueue-move";
+    String HQUEUE_REMOVE_CUZ_PLAYER_JOINED = "hgqueue-remove-cuz-player-joined";
 }
